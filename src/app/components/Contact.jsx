e28ea8cdf21385c39096a9de0f0437328728cfe8
EiichiM <matsuyoubi@gmail.com>
2023-10-15 02:42:49 -0600

    Contact update with powered, button dynimic prep

--- src/app/components/Contact.jsx
+++ src/app/components/Contact.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import VCard from "vcard-creator";
 import Image from "next/image";
+import ButtonAnimation from "./ButtonAnimation/ButtonAnimation";
+import SocialButtons from "./SocialButton/SocialButtons";
 
 import EmailIcon from "@mui/icons-material/Email";
 import PhoneIphoneIcon from "@mui/icons-material/PhoneIphone";
@@ -8,6 +10,7 @@ import PersonAddIcon from "@mui/icons-material/PersonAdd";
 import LanguageIcon from "@mui/icons-material/Language";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import PoweredButton from "./PoweredButton/PoweredButton";
 
 const Contact = ({
   altlogo,
@@ -20,6 +23,7 @@ const Contact = ({
   name,
   phone,
   title,
+  waMessage,
   webpage,
 }) => {
   const emailSubject = "Cotizacion desarollo";
@@ -58,87 +62,92 @@ const Contact = ({
   };
 
   return (
-    <div className="bg-gray-900 border-2 w-full min-h-screen mx-auto grid grid-rows-3 font-sans text-xl">
+    <div className="bg-gray-900 border-2 w-full min-h-screen mx-auto  font-sans text-xl pb-10">
       <main className="mt-8">
-        <div className="flex flex-col justify-center mt-8 items-center text-white">
+        <div className="flex flex-col justify-center items-center mt-8 text-white">
           <Image
-            src="/img/rpm_white.svg"
-            alt="rpm code"
-            className="w-12 h-12 mr-4"
-            width={100}
+            src="/rpm/rpm_white.svg"
+            alt={altlogo}
+            className="mb-4"
+            width={150}
             height={50}
           />
 
           <div className="mt-12 mb-12 w-48 h-48 border-6 border-teal-900 rounded-full">
             <Image
-              src="/img/eiichi.jpeg"
-              alt={alt}
+              src="/rpm/eiichi.jpeg"
+              alt={altImg}
               className="w-full h-full rounded-full"
               width={200}
               height={200}
             />
           </div>
         </div>
-        <div className={styles.Info_Container}>
-          <div className={styles.Title_Container}>
+        <div className="flex flex-col justify-center items-center text-white">
+          <div className="flex items-center justify-center w-full mb-8">
             <Image
-              src="/img/x_blanco.svg"
+              src="/rpm/x_blanco.svg"
               alt="rpm code"
-              className={styles.Title_Container_IMG}
+              className="mr-4"
               width={100}
               height={50}
             />
-            <h2 className="text-3xl font-semibold border-b-2 border-teal-400">
-              {title}
-            </h2>
+            <h2 className="text-3xl font-semibold border-b-2 ">{title}</h2>
           </div>
-          <div className="flex flex-col justify-center items-center text-white ml-12">
-            <div className="flex items-center mb-4">
-              <PhoneIphoneIcon className="text-pink-500 text-3xl" />
-              <a href={`tel:+52${phone}`} className="ml-4 text-lg">
-                {phone}
-              </a>
+          <div className="flex flex-col justify-center items-center text-white">
+            <div className="flex items-center my-4">
+              <ButtonAnimation
+                backgroundColor="#FB2B6D"
+                icon={<PhoneIphoneIcon className="text-white text-3xl mr-2" />}
+                label={phone}
+                link={`tel:+52${phone}`}
+              />
             </div>
-            <div className="flex items-center mb-4">
-              <WhatsAppIcon className="text-teal-500 text-3xl" />
-              <a
-                target="_blank"
-                href={urlWhatsApp}
-                className={styles.Info_Text1}
-              >
-                WhatsApp
-              </a>
+            <div className="flex items-center my-4">
+              <ButtonAnimation
+                backgroundColor="#90FAD6"
+                icon={<WhatsAppIcon className="text-white text-3xl mr-2" />}
+                label={`WhatsApp`}
+                link={urlWhatsApp}
+              />
             </div>
-            <div className="flex flex-col">
-              <div className="flex items-center mb-4">
-                <EmailIcon className="text-red-500 text-3xl" />
-              </div>
+            <div className="flex items-center my-4">
+              <ButtonAnimation
+                backgroundColor="#FB2B6D"
+                icon={<EmailIcon className="text-white text-3xl mr-2" />}
+                label={email}
+                link={emailPersonal}
+              />
+            </div>
+            <div className="flex items-center my-4">
+              <ButtonAnimation
+                backgroundColor="#FB2B6D"
+                icon={<EmailIcon className="text-white text-3xl mr-2" />}
+                label={emailC}
+                link={emailCompany}
+              />
+            </div>
+            <div className="flex items-center my-4">
+              <ButtonAnimation
+                backgroundColor="#90FAD6"
+                icon={<LanguageIcon className="text-white text-3xl mr-2" />}
+                label={webpage}
+                link={`https://${webpage}/`}
+              />
+            </div>
 
-              <div className="flex items-center mb-4">
-                <a href={emailPersonal} className="ml-4 text-lg">
-                  {email}
-                </a>
-                <a href={emailCompany} className="ml-4 text-lg">
-                  {emailC}
-                </a>
-              </div>
+            <div className="flex items-center my-4">
+              <SocialButtons />
+            </div>
+            <div className="flex items-center my-4">
+              <PoweredButton />
             </div>
             <div className="flex items-center mb-4">
-              <LanguageIcon className="text-teal-500 text-3xl" />
-              <a
-                href={`https://${webpage}/`}
-                target="_blank"
-                className="ml-4 text-lg"
-              >
-                {webpage}
-              </a>
-            </div>
-            <div className={styles.Info}>
               <GitHubIcon className="ml-4 text-lg" />
               <a
                 href="https://github.com/EiichiM"
                 target="_blank"
-                className={styles.Info_Text2}
+                className="ml-4 text-2xl"
               >
                 Git Hub
               </a>
@@ -148,9 +157,9 @@ const Contact = ({
       </main>
       <button
         onClick={downloadContact}
-        className="fixed border-2 border-white rounded-full w-20 h-20 bottom-8 right-8 flex justify-center items-center"
+        className="fixed border-2 border-white rounded-full w-20 h-20 bottom-8 right-8 flex justify-center items-center z-50"
       >
-        <PersonAddIcon className="text-teal-900 text-3xl" />
+        <PersonAddIcon className="text-white text-3xl " />
       </button>
     </div>
   );
