4bef62b8e0e7894d95af68c31c3885a36d72f8f9
EiichiM <matsuyoubi@gmail.com>
2023-10-15 02:35:50 -0600

    SEO config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const nextConfig = {
+    async headers() {
+        return [
+          {
+            source: "/favicon.ico",
+            headers: [
+              {
+                key: "Cache-Control",
+                value: "public, max-age=86400",
+              },
+            ],
+          },
+        ];
+      },
+    //   images: {
+    //     remotePatters: [
+    //       {
+    //         protocol: "https",
+    //         hostname: "card.rpmspeedcode.com",
+    //         post: "",
+    //         pathname: "/public/**",
+    //       },
+    //     ],
+    //   },
+}
 
 module.exports = nextConfig
