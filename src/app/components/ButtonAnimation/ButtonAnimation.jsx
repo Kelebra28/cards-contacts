107c12ee2c68ffe75dfb6bdcb874e9b293c108e9
Kelebra <ricardokelebra@gmail.com>
2023-10-17 02:40:22 -0600

    feat: fea: fear all dinamic

--- src/app/components/ButtonAnimation/ButtonAnimation.jsx
+++ src/app/components/ButtonAnimation/ButtonAnimation.jsx
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import "./ButtonAnimation.css"; // Import your CSS file
 
-const ButtonAnimation = ({ label, link, backgroundColor, icon }) => {
+const ButtonAnimation = ({ label, link, backgroundColor, icon, expand, textColor, negative }) => {
+  const [widthButton, setWidthButton] = useState(false)
     const backgroundStyle = {
-        "--background": backgroundColor
+        "--background": backgroundColor,
+        "--textColor": textColor,
    }
   
+   useEffect(() => {
+
+    if (label.length > 20 && expand) {
+      setWidthButton(!widthButton)
+    }
+
+   }, [label])
+
     return (
     <div className="container">
       <div
-        className="btn"
+        className={expand && widthButton ? 'btnL' : 'btn'}
         style={backgroundStyle}
       >
-        <a href={link}>
+        <a target="_blank" href={link}>
           {icon && <span className="icon">{icon}</span>}
           {label}
         </a>
